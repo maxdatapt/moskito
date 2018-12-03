d02914f12e8b86626f8400c8432523bc2b38d4fe
Enoque Duarte <enoque.duarte@maxdata.pt>
2018-12-03 11:21:47 +0000

    DiskSpaceProducer changed so it includes total disk space, free space and used percentage

--- moskito-extensions/moskito-disk-space-monitoring/src/main/java/net/anotheria/moskito/extensions/DiskSpaceMonitoringPlugin.java
+++ moskito-extensions/moskito-disk-space-monitoring/src/main/java/net/anotheria/moskito/extensions/DiskSpaceMonitoringPlugin.java
@@ -1,45 +1,50 @@
 package net.anotheria.moskito.extensions;
 
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.anotheria.moskito.core.plugins.AbstractMoskitoPlugin;
 import net.anotheria.moskito.core.registry.ProducerRegistryFactory;
 import net.anotheria.moskito.extensions.diskspacemonitoring.DiscSpaceMonitoringConfig;
 import net.anotheria.moskito.extensions.diskspacemonitoring.DiscSpaceProducer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
+import net.anotheria.moskito.extensions.diskspacemonitoring.DiscSpaceProducer.DiscSpaceInfoType;
 
 /**
  * Disk space monitoring plugin.
  */
 public class DiskSpaceMonitoringPlugin extends AbstractMoskitoPlugin {
 
-    /**
-     * Logger.
-     */
-    private static Logger log = LoggerFactory.getLogger(DiskSpaceMonitoringPlugin.class);
+	/**
+	 * Logger.
+	 */
+	private static Logger log = LoggerFactory.getLogger(DiskSpaceMonitoringPlugin.class);
 
-    @Override
-    public void initialize() {
-        DiscSpaceMonitoringConfig config = DiscSpaceMonitoringConfig.getInstance();
-        String[] disks = config.getDisks();
-		if (disks == null || disks.length==0)
+
+	@Override
+	public void initialize() {
+		DiscSpaceMonitoringConfig config = DiscSpaceMonitoringConfig.getInstance();
+		String[] disks = config.getDisks();
+		if (disks == null || disks.length == 0)
 			return;
-        for (Path root : FileSystems.getDefault().getRootDirectories()) {
-            for(String disk : disks) {
-                if (disk.equals(root.toString())) {
-                    log.info("" + ": ");
-                    ProducerRegistryFactory.getProducerRegistryInstance().registerProducer(new DiscSpaceProducer(root));
-                } else {
-                    log.info("Found unknown disk in config: " + disk);
-                }
-            }
-        }
-    }
+		for (Path root : FileSystems.getDefault().getRootDirectories()) {
+			for (String disk : disks) {
+				if (disk.equals(root.toString())) {
+					ProducerRegistryFactory.getProducerRegistryInstance().registerProducer(new DiscSpaceProducer(root, DiscSpaceInfoType.AVAILABLE));
+					ProducerRegistryFactory.getProducerRegistryInstance().registerProducer(new DiscSpaceProducer(root, DiscSpaceInfoType.TOTAL_SPACE));
+					ProducerRegistryFactory.getProducerRegistryInstance().registerProducer(new DiscSpaceProducer(root, DiscSpaceInfoType.USED_PCT));
+				} else {
+					log.info("Found unknown disk in config: " + disk);
+				}
+			}
+		}
+	}
 
-    @Override
-    public void deInitialize() {
-        super.deInitialize();
-    }
+
+	@Override
+	public void deInitialize() {
+		super.deInitialize();
+	}
 }
--- moskito-extensions/moskito-disk-space-monitoring/src/main/java/net/anotheria/moskito/extensions/diskspacemonitoring/DiscSpaceProducer.java
+++ moskito-extensions/moskito-disk-space-monitoring/src/main/java/net/anotheria/moskito/extensions/diskspacemonitoring/DiscSpaceProducer.java
@@ -1,13 +1,7 @@
 package net.anotheria.moskito.extensions.diskspacemonitoring;
 
-import net.anotheria.moskito.core.predefined.MemoryStats;
-import net.anotheria.moskito.core.producers.IStats;
-import net.anotheria.moskito.core.producers.IStatsProducer;
-import net.anotheria.moskito.core.util.BuiltinUpdater;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.FileStore;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,92 +9,144 @@ import java.util.List;
 import java.util.TimerTask;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.anotheria.moskito.core.predefined.MemoryStats;
+import net.anotheria.moskito.core.producers.IStats;
+import net.anotheria.moskito.core.producers.IStatsProducer;
+import net.anotheria.moskito.core.util.BuiltinUpdater;
+
 /**
  * DiscSpace producer.
  */
 public class DiscSpaceProducer implements IStatsProducer {
-    /**
-     * Logger.
-     */
-    private static Logger log = LoggerFactory.getLogger(DiscSpaceProducer.class);
-    /**
-     * The id of the producer.
-     */
-    private String path;
-    /**
-     * The id of the producer.
-     */
-    private String producerId;
-    /**
-     * Stats.
-     */
-    private MemoryStats stats;
-    /**
-     * Cached stats list.
-     */
-    private List<IStats> statsList;
+	/**
+	 * Logger.
+	 */
+	private static Logger log = LoggerFactory.getLogger(DiscSpaceProducer.class);
+	/**
+	 * The id of the producer.
+	 */
+	private String path;
+	/**
+	 * The id of the producer.
+	 */
+	private String producerId;
+	/**
+	 * Stats.
+	 */
+	private MemoryStats stats;
+	/**
+	 * Cached stats list.
+	 */
+	private List<IStats> statsList;
 
-    private static final String ID = "DiscSpaceUsable. path - ";
+	private static final String AVAILABLE_ID = "DiscSpaceUsable on - ";
+	private static final String TOTAL_ID = "DiscSpaceTotal on - ";
+	private static final String USED_PCT = "DiscSpaceUsedPct on - ";
 
-    private FileStore root;
+	private FileStore root;
 
-    public DiscSpaceProducer(Path rootPath){
-        path = rootPath.toString();
-        producerId = ID + path;
-        statsList = new CopyOnWriteArrayList<IStats>();
-        stats = new MemoryStats(producerId);
-        statsList.add(stats);
+	private DiscSpaceInfoType discInfoType;
 
-        try {
-            root = Files.getFileStore(rootPath);
-        } catch (IOException e) {
-            log.error("Querying space. Init error: " + e.toString());
-            return;
-        }
+	public enum DiscSpaceInfoType {
+		AVAILABLE,
+		TOTAL_SPACE,
+		USED_PCT;
+	}
 
-        BuiltinUpdater.addTask(new TimerTask() {
-            @Override
-            public void run() {
-                readMemory();
-            }
-        });
-    }
 
-    @Override
-    public String getCategory() {
-        return "memory";
-    }
+	public DiscSpaceProducer(Path rootPath, DiscSpaceInfoType discInfoType) {
+		path = rootPath.toString();
 
-    @Override
-    public String getProducerId() {
-        return producerId;
-    }
+		this.discInfoType = discInfoType;
+		switch (discInfoType) {
+			case USED_PCT:
+				producerId = USED_PCT + path;
+				break;
+			case TOTAL_SPACE:
+				producerId = TOTAL_ID + path;
+				break;
+			case AVAILABLE:
+			default:
+				producerId = AVAILABLE_ID + path;
+				break;
+		}
 
-    @Override
-    public List<IStats> getStats() {
-        return statsList;
-    }
+		statsList = new CopyOnWriteArrayList<IStats>();
+		stats = new MemoryStats(producerId);
+		statsList.add(stats);
 
-    @Override
-    public String getSubsystem() {
-        return "plugins";
-    }
+		try {
+			root = Files.getFileStore(rootPath);
+		} catch (IOException e) {
+			log.error("Querying space. Init error: " + e.toString());
+			return;
+		}
 
-    /**
-     * Reads the memory value from the resolver and updates internal stats.
-     */
-    private void readMemory() {
-        long space;
-        try {
-            space = root.getUsableSpace();
-        } catch (IOException e) {
-            log.error("Querying space. Querying error: " + e.toString());
-            return;
-        }
-        stats.updateMemoryValue(space);
-    }
+		BuiltinUpdater.addTask(new TimerTask() {
+			@Override
+			public void run() {
+				readMemory();
+			}
+		});
+	}
 
-    @Override public String toString(){
-        return super.toString()+ ' ' +this.getClass().getSimpleName();
-    }
+
+	@Override
+	public String getCategory() {
+		return "memory";
+	}
+
+
+	@Override
+	public String getProducerId() {
+		return producerId;
+	}
+
+
+	@Override
+	public List<IStats> getStats() {
+		return statsList;
+	}
+
+
+	@Override
+	public String getSubsystem() {
+		return "plugins";
+	}
+
+
+	/**
+	 * Reads the memory value from the resolver and updates internal stats.
+	 */
+	private void readMemory() {
+		long space;
+		try {
+			switch (discInfoType) {
+				case USED_PCT:
+					space = new BigDecimal(root.getUsableSpace()).multiply(new BigDecimal(100)).divide(new BigDecimal(root.getTotalSpace()), 0).longValue();
+					break;
+				case TOTAL_SPACE:
+					space = root.getTotalSpace();
+					break;
+				case AVAILABLE:
+				default:
+					space = root.getUsableSpace();
+					producerId = AVAILABLE_ID + path;
+					break;
+			}
+		} catch (IOException e) {
+			log.error("Querying space. Querying error: " + e.toString());
+			return;
+		}
+		stats.updateMemoryValue(space);
+	}
+
+
+	@Override
+	public String toString() {
+		return super.toString() + ' ' + this.getClass().getSimpleName();
+	}
 }
